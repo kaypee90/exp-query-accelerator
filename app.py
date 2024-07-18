98417eb8ef0eab4b8f367e3631a6ab19f80f1ccb
kaypee90 <kaypee90@yahoo.com>
2024-07-18 15:25:14 -0400

    Update doc string

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-# Author kaypee90
+"""
+Author kaypee90
 
-# TTL
-# Separate Query Cache and Item Cache
-# Based on async await
-# Handle concurrency
-# Monitoring tool eg. Displaying Service health
+TTL
+Separate Query Cache and Item Cache
+Based on async await
+Handle concurrency
+Monitoring tool eg. Displaying Service health
+Use official db async driver
 
-#  ==============================================
+==============================================
+"""
 
 import uvicorn
 from handlers import handle_invalid_request, handle_post
