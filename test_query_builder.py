2150195fb108ceb3536a716d7647087435462365
kaypee90 <kaypee90@yahoo.com>
2024-10-20 20:01:03 -0400

    Group test case using classes

--- test_query_builder.py
+++ test_query_builder.py
@@ -1,40 +1,41 @@
 import pytest
 from db.query_builder import build_sql_query
 
-
-def test_build_sql_query_with_no_filters():
-    query = build_sql_query("table")
-    assert query == "SELECT * FROM table"
-
-
-def test_build_sql_query_with_filters_and_fields():
-    query = build_sql_query(
-        "table",
-        fields=["name", "price"],
-        filters=[{"field": "price", "value": "30", "operator": "="}],
-    )
-    assert query == "SELECT name, price FROM table WHERE price = '30'"
-
-
-def test_build_sql_query_with_no_table():
-    with pytest.raises(AssertionError):
-        build_sql_query(
-            "",
+class TestQueryBuilder:
+    
+    def test_build_sql_query_with_no_filters(self):
+        query = build_sql_query("table")
+        assert query == "SELECT * FROM table"
+    
+    
+    def test_build_sql_query_with_filters_and_fields(self):
+        query = build_sql_query(
+            "table",
             fields=["name", "price"],
             filters=[{"field": "price", "value": "30", "operator": "="}],
         )
-
-
-def test_build_sql_query_with_multiple_filters_and_fields():
-    query = build_sql_query(
-        "table",
-        fields=["name", "price"],
-        filters=[
-            {"field": "price", "value": "30%", "operator": "LIKE"},
-            {"field": "name", "value": "Lisa Smith", "operator": "=", "bind": "|"},
-        ],
-    )
-    assert (
-        query
-        == "SELECT name, price FROM table WHERE price LIKE '30%' OR name = 'Lisa Smith'"
-    )
+        assert query == "SELECT name, price FROM table WHERE price = '30'"
+    
+    
+    def test_build_sql_query_with_no_table(self):
+        with pytest.raises(AssertionError):
+            build_sql_query(
+                "",
+                fields=["name", "price"],
+                filters=[{"field": "price", "value": "30", "operator": "="}],
+            )
+    
+    
+    def test_build_sql_query_with_multiple_filters_and_fields(self):
+        query = build_sql_query(
+            "table",
+            fields=["name", "price"],
+            filters=[
+                {"field": "price", "value": "30%", "operator": "LIKE"},
+                {"field": "name", "value": "Lisa Smith", "operator": "=", "bind": "|"},
+            ],
+        )
+        assert (
+            query
+            == "SELECT name, price FROM table WHERE price LIKE '30%' OR name = 'Lisa Smith'"
+        )
--- test_validator.py
+++ test_validator.py
@@ -1,33 +1,33 @@
 from validator import validate_payload
 
+class TestValidator:
 
-def test_validate_payload_without_table():
-    errors = validate_payload({})
-    assert errors[0] == "`table` is required"
+    def test_validate_payload_without_table(self):
+        errors = validate_payload({})
+        assert errors[0] == "`table` is required"
 
+    def test_validate_payload_with_non_list_fields(self):
+        errors = validate_payload({"table": "test", "fields": "test"})
+        assert errors[0] == "`fields` must be a list"
+    
 
-def test_validate_payload_with_non_list_fields():
-    errors = validate_payload({"table": "test", "fields": "test"})
-    assert errors[0] == "`fields` must be a list"
-
-
-def test_validate_payload_with_fields_with_list_not_containing_all_strings():
-    errors = validate_payload({"table": "test", "fields": ["test", 1]})
-    assert errors[0] == "`fields` must be a list of strings"
-
-
-def test_validate_payload_with_fields_with_list_containing_all_strings():
-    errors = validate_payload({"table": "test", "fields": ["test", "test2"]})
-    assert len(errors) == 0
-
-
-def test_validate_payload_with_filters_not_list():
-    errors = validate_payload({"table": "test", "filters": "test"})
-    assert errors[0] == "`filters` must be a list"
-
-
-def test_validate_payload_with_filters_must_have_field_value_as_string():
-    errors = validate_payload(
-        {"table": "test", "filters": [{"field": 1, "value": "test", "operator": "="}]}
-    )
-    assert errors[0] == "`filters` keys must be strings"
+    def test_validate_payload_with_fields_with_list_not_containing_all_strings(self):
+        errors = validate_payload({"table": "test", "fields": ["test", 1]})
+        assert errors[0] == "`fields` must be a list of strings"
+    
+    
+    def test_validate_payload_with_fields_with_list_containing_all_strings(self):
+        errors = validate_payload({"table": "test", "fields": ["test", "test2"]})
+        assert len(errors) == 0
+    
+    
+    def test_validate_payload_with_filters_not_list(self):
+        errors = validate_payload({"table": "test", "filters": "test"})
+        assert errors[0] == "`filters` must be a list"
+    
+    
+    def test_validate_payload_with_filters_must_have_field_value_as_string(self):
+        errors = validate_payload(
+            {"table": "test", "filters": [{"field": 1, "value": "test", "operator": "="}]}
+        )
+        assert errors[0] == "`filters` keys must be strings"
